869a902cdb26a65f8ee2d1dded4e4fe9f5fa4c00
Juan Pablo C. Mejia <jpablocardona@gmail.com>
2020-03-30 22:46:47 -0500

    finalizando web scraping

--- web-scrapper-course-data-eng/main.py
+++ web-scrapper-course-data-eng/main.py
@@ -1,9 +1,20 @@
-from common import config
 import argparse
 import logging
+import datetime
+import csv
+import re
 import news_page_objects as news
+
+from common import config
+from news_page_objects import ArticlePage
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
+
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+is_well_formed_link = re.compile(r'^https?://.+/.+$')
+is_root_path = re.compile(r'^/.+$')
 
 
 def _news_scraper(news_site_uid):
@@ -11,8 +22,54 @@ def _news_scraper(news_site_uid):
     logger.info('Beginning scraper for {}'.format(host))
     homepage = news.HomePage(news_site_uid, host)
 
+    articles = []
     for link in homepage.article_links:
-        print(link)
+        article = _fetch_article(news_site_uid, host, link)
+
+        if article:
+            logger.info('Article fetched!!!')
+            articles.append(article)
+            break
+
+    _save_articles(news_site_uid, articles)
+
+
+def _save_articles(news_site_uid, articles):
+    now = datetime.datetime.now().strftime('%Y_%m_%d')
+    out_filename = '{}_{}_articles.csv'.format(news_site_uid, now)
+    csv_headers = list(
+        filter(lambda property: not property.startswith('_'), dir(articles[0])))
+
+    with open(out_filename, mode='w+') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+        for article in articles:
+            row = [str(getattr(article, prop)) for prop in csv_headers]
+            writer.writerow(row)
+
+
+def _fetch_article(news_site_uid, host, link):
+    logger.info('Start fetching article at {}'.format(link))
+    article = None
+    try:
+        article = ArticlePage(news_site_uid, _build_link(host, link))
+    except (HTTPError, MaxRetryError) as e:
+        logger.warning('Error while fetching the article ', exc_info=False)
+
+    if article and not article.body:
+        logger.warning('Invalid article, There is not body')
+        return None
+
+    return article
+
+
+def _build_link(host, link):
+    if is_well_formed_link.match(link):
+        return link
+    elif is_root_path.match(link):
+        return '{}{}'.format(host, link)
+    else:
+        return '{}{}'.format(host, link)
 
 
 if __name__ == '__main__':
